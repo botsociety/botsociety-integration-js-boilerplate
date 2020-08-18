0d35ca93b1722447d7ff5cf5f7fc63959da62f54
Vittorio Erlapso Banfi <erlapso@users.noreply.github.com>
2020-08-17 18:34:33 -0700

    added promise

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 const { prop } = require('ramda/src')
 
-const triggerExport = async(event) => {
+const triggerExport = async (event) => {
   const headers = prop('headers', event)
   const accessToken = prop('Access-Token', headers)
   const { user_id, api_key_public } = headers
@@ -16,20 +16,24 @@ const triggerExport = async(event) => {
     }
     
     The callbackUrl is a URL you can call in async if your integration needs to run for some seconds.
-    It will notify the Botsociety user that the export is complete and available
+    It will notify the Botsociety user that the export is complete and available.
+    More info at Check out https://botsociety.io/documentation/custom-integrations/#developing-your-custom-integration 
   */
- 
+
   /* Here you can do your elaborations, for example calling the Botsociety API with the user_id and api_key_public */
 
-  /* Once you are done with the immediate checks, you need to give a reponse to the POST */
-  var statusCode = 200;
-  var info = {message: "The export succeded"};
-  return {
-    statusCode,
-    body: JSON.stringify({
-      info,
-    }),
-  }
+  /* If you need to call a third party API, or to create a file, you can use a promise here */
+  const main = new Promise(function (resolve, reject) {
+    var statusCode = 200;
+    var info = { message: "The export succeded" };
+    resolve({
+      statusCode,
+      body: JSON.stringify({
+        info,
+      }),
+    })
+  })
+  return main;
 }
 
 module.exports = {
